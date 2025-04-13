99a97ed3aa7a8355cccbb69d28cb2884840ce84a
Prateek <chawlaprateek89@gmail.com>
2025-04-13 14:01:44 +0530

    Assignment 3

---
+++ src/assignments/Asignment3_EmpGroups.java
@@ -0,0 +1,24 @@
+package assignments;
+
+public class Asignment3_EmpGroups {
+
+	public static void main(String[] args) {
+Assignment3_Employees emp = new Assignment3_Employees();
+
+String[] empNamee = new String[3];
+empNamee[0] =emp.employee1;
+empNamee[1] =emp.employee2;
+empNamee[2] =emp.employee3;
+
+int[] empInt = new int[3];
+empInt[0]=emp.empid1;
+empInt[1]=emp.empid2;
+empInt[2]=emp.empid3;
+
+
+
+
+		
+	}
+
+}
---
+++ src/assignments/Assignment3_Employees.java
@@ -0,0 +1,21 @@
+package assignments;
+
+public class Assignment3_Employees {
+
+
+		
+		String employee1="Bharath";
+		String employee2="ABC";
+		String employee3="XYZ";
+
+		int empid1=1234;
+		int empid2=1235;
+		int empid3=1236;
+		
+		
+}
+		
+
+	
+
+
